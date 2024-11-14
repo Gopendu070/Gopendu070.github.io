9b47e1d6a291d5a803b9c6363e43406ff24b3f6e
gopendu <gopendumanna@gmail.com>
2024-11-14 08:48:34 +0530

    ready for deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2397516596"
+    serviceWorkerVersion: "2181077817"
   }
 });
